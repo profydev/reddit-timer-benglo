c40546e064a6a5470b3d005e4f7bf3d8c9d892c7
Ben <7553969+benglo@users.noreply.github.com>
2022-02-18 11:11:48 +1000

    Created folder structure for pages and components. React-router installed New routes added to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div>App Placeholder</div>;
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Routes>
+        <Route path="/" element="" />
+        <Route path="/search" element="" />
+        <Route path="/how" element="" />
+        <Route path="/about" element="" />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
